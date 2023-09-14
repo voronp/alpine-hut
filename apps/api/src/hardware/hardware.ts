77af7309961df9d5ae39dedd90ccad6d2b04bca4
Pavel Voronov <p.voronov@bftcom.com>
2023-09-14 22:53:24 +0300

    debug

--- apps/api/src/hardware/hardware.ts
+++ apps/api/src/hardware/hardware.ts
@@ -6,12 +6,15 @@ import rpio from 'rpio';
 @Injectable()
 export class Hardware extends AbstractHardware {
   openPIO(pio:number, mode:string, option?:string) {
+    console.trace('openPIO');
     rpio.open(pio, mode, option);
   }
   setHighPIO(pio:number) {
+    console.trace('setHighPIO');
     rpio.write(pio, AbstractHardware.HIGH);
   }
   setLowPIO(pio:number) {
+    console.trace('setLowPIO');
     rpio.write(pio, AbstractHardware.LOW);
   }
   async getTemperature(id:string):Promise<number> {
