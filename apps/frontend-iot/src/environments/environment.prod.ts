4d07f6da79285e21fb63209a941d4541e635c39f
Pavel Voronov <p.voronov@bftcom.com>
2023-09-14 22:13:04 +0300

    prod setup stuff

--- apps/frontend-iot/src/environments/environment.prod.ts
+++ apps/frontend-iot/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true,
+  production: false,
+  gqlUrl: 'http://192.168.1.33:3333/graphql',
+  wsUrl: 'ws://192.168.1.33:3333/graphql',
+  httpUrl: 'http://192.168.1.33:3333',
 };
